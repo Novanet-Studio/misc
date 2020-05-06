a47621b4f26c1a533976019baaa017df2e4846d3
Davejs136 <davejs136@gmail.com>
2020-05-06 13:16:49 -0400

    Check the form when is finished, constructor added

--- table.js
+++ table.js
@@ -1,6 +1,38 @@
 class Table {
   constructor() {
-    this.phrase = 'Poco'
+    this.phrase = 'Para avanzar a la siguiente etapa, necesitas comentar el post diciendo con cuál dios de la mitología relacionas a Novanet y por qué. Busca información sobre nosotros en nuestro sitio web. Debes comenzar tu comentario con la palabra hey'
+    this.total = []
+    
+    const regexpr = /[a-z|á|é|í|ó|ú]+/gi
+
+    // Reemplazo las letras con tilde
+    this.phrase = this.phrase.replace(/[á|é|í|ó|ú]/g, (x) => {
+      switch (x) {
+        case 'á':
+          return 'a'
+          break
+        case 'é':
+          return 'e'
+          break
+        case 'í':
+          return 'i'
+        break
+        case 'ó':
+          return 'o'
+        break
+        case 'ú':
+          return 'u'
+        break
+      }
+    })
+
+    // Asigno todas las letras a un arreglo
+    this.phrase.match(regexpr).forEach(word => 
+      word === 'Novanet' || 
+      word.toLowerCase().split('').forEach(letter => 
+        this.total.push(letter)
+      )
+    )
   }
   /* -------------------- Private -------------------- */
   _generateTables(rows, word = false) {
@@ -46,6 +78,7 @@ class Table {
       // Asigno el contenido
       span.textContent = el.textContent
       input.value = span.textContent
+      input.maxLength = 1 // un caracter
 
       form.appendChild(input)
       form.style.display = 'inline-block'
@@ -59,11 +92,36 @@ class Table {
       form.addEventListener('submit', e => {
         e.preventDefault()
         updateDisplay()
+        this.checkPhrase()
       })
 
-      input.addEventListener('blur', updateDisplay)
+      input.addEventListener('blur', e => {
+        updateDisplay()
+        this.checkPhrase()
+      })
     } else {
-      console.log('porsupollo')
+      const form = el.children[0]
+      const input = el.children[0].children[0]
+      const span = el.children[1]
+
+      input.value = span.textContent 
+      form.style.display = 'inline-block'
+      span.style.display = 'none'
+
+      input.style.width = '40px'
+      input.focus()
+
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        updateDisplay()
+        this.checkPhrase()
+      })
+
+
+      input.addEventListener('blur', e => {
+        updateDisplay()
+        this.checkPhrase()
+      })
     }
 
     // Helper
@@ -72,6 +130,22 @@ class Table {
       form.style.display = 'none'
       span.textContent = input.value.toUpperCase()
       input.style.width = `${span.clientWidth}px`
+    }
+  }
+
+  checkPhrase() {
+    const span = document.querySelectorAll('p > span')
+    const letters = []
+    
+    Array.from(span).forEach(item => 
+      item.textContent && letters.push(item.textContent)
+    )
+
+    console.log(letters)
+
+    if (letters.length === this.total.length) {
+      letters.join('').toLowerCase() === this.total.join('') && 
+      alert('Lo hiciste') // aqui acciona el modal de success
     }
   }
 
